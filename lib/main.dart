8369d4faafe58add23c24e2ea66c1bef3ea2dc84
mrtucar <mrtucar@msn.com>
2021-12-29 15:39:11 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+import 'package:flutterapp2/screens/todoList.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Todos Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: TodoList(),
+    );
+  }
+}
